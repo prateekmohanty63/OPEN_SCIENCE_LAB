031cb0766d940e6f0d5503b007cd018f96b6d674
Prateek <prateekmohanty63@gmail.com>
2023-02-13 17:13:48 +0530

    Deploying to railways

--- researchLab/researchLab/settings.py
+++ researchLab/researchLab/settings.py
@@ -180,9 +180,13 @@ EMAIL_HOST_PASSWORD = 'ozudkymorpbsvrzi'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATIC_URL='/static/'
-STATICFILES_DIRS=[BASE_DIR/"static"]
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
 # ozudkymorpbsvrzi
